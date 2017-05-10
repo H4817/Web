99e77294f2d3947dfe4e2b196b27898f8a710378
nikolaj <jakimychev.nikolaj@gmail.com>
2017-05-10 17:38:02 +0300

    add index.php

---
+++ Lab2/index.php
@@ -0,0 +1,11 @@
+
+<script type="text/javascript" src="Shape.js"></script>
+<script type="text/javascript" src="Circle.js"></script>
+<script type="text/javascript" src="Triangle.js"></script>
+<script type="text/javascript" src="Rectangle.js"></script>
+
+<script type="text/javascript">
+    PrintCircle();
+    PrintTriangle();
+    PrintRectangle();
+</script>
